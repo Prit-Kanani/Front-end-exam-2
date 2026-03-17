b1a04db1741591c9eb6972408a744b6bedc44ac9
Prit Kanani <kananiprit0@gmail.com>
2026-03-17 11:43:27 +0530

    small changes

--- Front-end-exam/Models/ErrorViewModel.cs
+++ Front-end-exam/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace Front_end_exam.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace Front_end_exam.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
--- Front-end-exam/Services/ClinicCmsApiClient.cs
+++ Front-end-exam/Services/ClinicCmsApiClient.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
+using Front_end_exam.Models;
 using System.Net;
-using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
-using Front_end_exam.Models;
 
 namespace Front_end_exam.Services;
 
